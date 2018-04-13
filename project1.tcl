c33659c8c36d4b05fe43fa1ea263d7716469a1a6
Judah Perez <tythin4@gmail.com>
2018-04-13 12:49:52 -0700

    maybe can run?

--- project1.tcl
+++ project1.tcl
@@ -18,6 +18,16 @@ set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
 set n7 [$ns node]
+set n8 [$ns node]
+set n9 [$ns node]
+set n10 [$ns node]
+set n11 [$ns node]
+set n12 [$ns node]
+set n13 [$ns node]
+set n14 [$ns node]
+set n15 [$ns node]
+set n16 [$ns node]
+set n17 [$ns node]
 
 #Create duplex links between the nodes
 $ns duplex-link $n0 $n2 2Mb 40ms DropTail
@@ -28,6 +38,16 @@ $ns duplex-link $n3 $n4 8Mb 100ms DropTail
 $ns duplex-link $n3 $n5 2Mb 40ms DropTail
 $ns duplex-link $n4 $n6 2Mb 40ms DropTail
 $ns duplex-link $n4 $n7 2Mb 40ms DropTail
+$ns duplex-link $n8 $n0 1Mb 20ms DropTail
+$ns duplex-link $n9 $n1 1Mb 20ms DropTail
+$ns duplex-link $n10 $n5 1Mb 20ms DropTail
+$ns duplex-link $n11 $n5 1Mb 20ms DropTail
+$ns duplex-link $n12 $n5 1Mb 20ms DropTail
+$ns duplex-link $n13 $n5 1Mb 20ms DropTail
+$ns duplex-link $n14 $n6 1Mb 20ms DropTail
+$ns duplex-link $n15 $n6 1Mb 20ms DropTail
+$ns duplex-link $n16 $n7 1Mb 20ms DropTail
+$ns duplex-link $n17 $n7 1Mb 20ms DropTail
 
 #Set Queue Size of links
 $ns queue-limit $n0 $n2 25
@@ -38,24 +58,33 @@ $ns queue-limit $n3 $n4 30
 $ns queue-limit $n3 $n5 25
 $ns queue-limit $n4 $n6 25
 $ns queue-limit $n4 $n7 25
+$ns queue-limit $n9 $n1 20
+$ns queue-limit $n10 $n5 20
+$ns queue-limit $n11 $n5 20
+$ns queue-limit $n12 $n5 20
+$ns queue-limit $n13 $n5 20
+$ns queue-limit $n14 $n6 20
+$ns queue-limit $n15 $n6 20
+$ns queue-limit $n16 $n7 20
+$ns queue-limit $n17 $n7 20
 
 #Create a UDP agent and attach it to node n0
 set tcp8to10 [new Agent/TCP]
-$ns attach-agent $n0 $tcp8to10
+$ns attach-agent $n8 $tcp8to10
 set tcp8to11 [new Agent/TCP]
-$ns attach-agent $n0 $tcp8to11
+$ns attach-agent $n8 $tcp8to11
 set udp9to12 [new Agent/UDP]
-$ns attach-agent $n1 $udp9to12
+$ns attach-agent $n9 $udp9to12
 set udp9to13 [new Agent/UDP]
-$ns attach-agent $n1 $udp9to13
+$ns attach-agent $n9 $udp9to13
 set tcp8to14 [new Agent/TCP]
-$ns attach-agent $n0 $tcp8to14
+$ns attach-agent $n8 $tcp8to14
 set udp9to15 [new Agent/UDP]
-$ns attach-agent $n1 $udp9to15
+$ns attach-agent $n9 $udp9to15
 set tcp8to16 [new Agent/TCP]
-$ns attach-agent $n0 $tcp8to16
+$ns attach-agent $n8 $tcp8to16
 set udp9to17 [new Agent/UDP]
-$ns attach-agent $n1 $udp9to17
+$ns attach-agent $n9 $udp9to17
 
 #Set flow id for each traffic
 $tcp8to10 set fid_ 0
@@ -118,30 +147,52 @@ $cbr9To17 attach-agent $udp9to17
 
 #Create a Null agent (a traffic sink) and attach it to node
 set null10 [new Agent/Null]
-$ns attach-agent $n5 $null10
+$ns attach-agent $n10 $null10
 set null11 [new Agent/Null]
-$ns attach-agent $n5 $null11
+$ns attach-agent $n11 $null11
 set null12 [new Agent/Null]
-$ns attach-agent $n5 $null12
+$ns attach-agent $n12 $null12
 set null13 [new Agent/Null]
-$ns attach-agent $n6 $null13
+$ns attach-agent $n13 $null13
 set null14 [new Agent/Null]
-$ns attach-agent $n6 $null14
+$ns attach-agent $n14 $null14
 set null15 [new Agent/Null]
-$ns attach-agent $n5 $null15
+$ns attach-agent $n15 $null15
 set null16 [new Agent/Null]
-$ns attach-agent $n7 $null16
+$ns attach-agent $n16 $null16
 set null17 [new Agent/Null]
-$ns attach-agent $n7 $null17
+$ns attach-agent $n17 $null17
 #Connect the traffic source with the traffic sink
 $ns connect $tcp8to10 $null10
 $ns connect $tcp8to11 $null11
 $ns connect $udp9to12 $null12
 $ns connect $udp9to13 $null13
+$ns connect $tcp8to14 $null14
+$ns connect $udp9to15 $null15
+$ns connect $tcp8to14 $null16
+$ns connect $udp9to15 $null17
 
 #Schedule events for the CBR agent
 $ns at 1 "$cbr8To10 start"
 $ns at 10 "$cbr8To10 stop"
+$ns at 1 "$cbr8To11 start"
+$ns at 10 "$cbr8To11 stop"
+$ns at 2 "$cbr9To12 start"
+$ns at 10 "$cbr9To12 stop"
+$ns at 2 "$cbr9To13 start"
+$ns at 10 "$cbr9To13 stop"
+$ns at 1 "$cbr8To14 start"
+$ns at 10 "$cbr8To14 stop"
+$ns at 2 "$cbr9To15 start"
+$ns at 10 "$cbr9To15 stop"
+$ns at 1 "$cbr8To16 start"
+$ns at 10 "$cbr8To16 stop"
+$ns at 2 "$cbr9To17 start"
+$ns at 10 "$cbr9To17 stop"
+
+#Schedule events for the CBR agent and the network dynamics
+$ns rtmodel-at 6.0 down $n2 $n3
+$ns rtmodel-at 7.0 up $n2 $n3
 
 $ns at 10 "exit 0"
 
